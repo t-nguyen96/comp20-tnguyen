a3ccca7279fdf47bb7c29387b5431b1ce4335864
t-nguyen96 <anthony.tony.nguyen96@gmail.com>
2016-03-11 22:45:20 -0500

    finished lab6

---
+++ messages/README.md
@@ -0,0 +1,11 @@
+#lab6 README
+
+Correctly Implemented:
+* This lab has been correctly implemented. The URI does not because of same origin policy,
+  but the data.json file works because it is within the same directory.
+* Messages print if the data.json file works
+
+Hours spent: 1/2 of an hour
+
+It is not possible because of the Same Origin Policy unless you modify the server
+to ignore the policy.
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,21 @@
 // Your JavaScript goes here...
+
+function parse() {
+    var request = new XMLHttpRequest();
+    //var url = "data.json";
+    var url = "https://messagehub.herokuapp.com/messages.json";
+    request.open("GET", url, true);
+    message_box = document.getElementById("messages");
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            raw_mess = request.responseText;
+            message = JSON.parse(raw_mess);
+            message_box.innerHTML = "<p>" + message[0].content + " - " 
+                                    + message[0].username + "</p>";
+            message_box.innerHTML = "<p>" + message[1].content + " - " 
+                                    + message[1].username + "</p>" + message_box.innerHTML;
+        }
+    }
+    request.send(null);
+}
